2688168b21559846c6399bd1ee3072f103b7238b
Muggle Wang <wangyufeithu@outlook.com>
2018-06-28 15:29:18 +0800

    new version

--- net.py
+++ net.py
@@ -2,6 +2,75 @@ import torch
 import torch.nn as nn
 
 
+# ---------------------------------------------- new version ----------------------------------------------
+# Simpler and more scalable
+# You can change the network to sphere64 by changing the variables sphere.layers
+class Block(nn.Module):
+    def __init__(self, planes):
+        super(Block, self).__init__()
+        self.conv1 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.prelu1 = nn.PReLU(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.prelu2 = nn.PReLU(planes)
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.prelu1(out)
+        out = self.conv2(out)
+        out = self.prelu2(out)
+        out += residual
+
+        return out
+
+
+class sphere(nn.Module):
+    def __init__(self, is_gray=False):
+        super(sphere, self).__init__()
+        block = Block
+        layers = [1, 2, 4, 1]
+        filter_list = [3,64,128,256,512]
+        if is_gray:
+            filter_list[0] = 1
+
+        self.layer1 = self._make_layer(block, filter_list[0], filter_list[1], layers[0], stride=2)
+        self.layer2 = self._make_layer(block, filter_list[1], filter_list[2], layers[1], stride=2)
+        self.layer3 = self._make_layer(block, filter_list[2], filter_list[3], layers[2], stride=2)
+        self.layer4 = self._make_layer(block, filter_list[3], filter_list[4], layers[3], stride=2)
+        self.fc = nn.Linear(512 * 7 * 6, 512)
+
+        # Weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.xavier_uniform(m.weight)
+                if m.bias is not None:
+                    nn.init.constant(m.bias, 0.0)
+
+    def _make_layer(self, block, inplanes, planes, blocks, stride):
+        layers = []
+        layers.append(nn.Conv2d(inplanes, planes, 3, stride, 1))
+        layers.append(nn.PReLU(planes))
+        for i in range(blocks):
+            layers.append(block(planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+    def save(self, file_path):
+        with open(file_path, 'wb') as f:
+            torch.save(self.state_dict(), f)
+
+# ---------------------------------------------- old version ----------------------------------------------
 class sphere20(nn.Module):
     def __init__(self):
         super(sphere20, self).__init__()
